c9cb66ba0779c36ff0cf8af9ff5da46c1cee6824
mahayasa <70327689+mahayasa1@users.noreply.github.com>
2026-09-19 07:40:04 +0800

    update rules pass

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,13 +38,8 @@ class AppServiceProvider extends ServiceProvider
         );
 
         Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null,
+            ? Password::min(8)
+            : null
         );
     }
 }
